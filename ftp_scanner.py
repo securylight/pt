43dbcb96648c1157781c5e8e4c5a53c0cc5f6365
Or <37718207+securylight@users.noreply.github.com>
2019-08-18 18:49:35 +0300

    Update ftp_scanner.py

--- ftp_scanner.py
+++ ftp_scanner.py
@@ -2,16 +2,38 @@ from ftplib import FTP
 
 
 
+f= open("results.txt","a+")
 
 with open("ftp_hosts1.txt") as file:
 	for line in file:
-		line = line.strip() #preprocess line
+		line = line.strip() 
 		try:
-			print("Try to connect to: {}".format(line))
+			print("Try to connect to: {}, anonymously".format(line))
 			ftp = FTP(line, timeout=3)
 
 		except:
 			print("connection to {} failed!".format(line))
 		else:
 			print("connection to {} successed!".format(line))
+			f.write("connection to {} anonymously successed!".format(line))
 
+		print("Try to connect to: {}, with username and password".format(line))
+	
+		
+		with open("A-List-Users-Short.txt") as file:
+			for user in file:
+				user = user.strip() 
+				#print user
+				with open("A-List-passwords.txt") as file2:
+					for password in file2:
+						#print ("user:{}, password:{}".format(user, password))
+							try:
+								ftp = FTP(line,user,password, timeout=3)
+
+							except:
+								print("connection to {} with user: {} and password: {} failed!".format(line, user, password))
+							else:
+								print("connection to {} with user: {} and password: {} successed!".format(line, user, passsword))
+								f.write("connection to {} with user: {} and password: {} successed!".format(line, user, passsword))
+								
+f.close()
